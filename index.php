67d349313b92f3a486b9f6efdd9a649fd1ed8c0b
Carlucci Alberto <albertps3@hotmail.it>
2020-09-09 19:01:36 +0200

    inizio

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pharmacare</title>
+    <link rel="stylesheet" href="build/css/main.css">
+    <link rel="stylesheet" href="build/css/style.css">
+    <link rel="stylesheet" href="build/css/bootstrap.css">
+    <script src="https://kit.fontawesome.com/9efad85de0.js" crossorigin="anonymous"></script>
+</head>
+
+
+<body >
+
+         <div class="container-fluid">
+             <div class="row">
+         <?php include 'html/header.html'; ?>
+         </div>
+                   
+                   
+                    <div class="masthead" id="landscape">
+  <div class="container h-100">
+    <div class="row h-100 align-items-center">
+     
+      <img src="Img/Home.jpg" class="img-fluid text-center pl-3">
+      
+    </div>
+</div>                   </div>
+</div>
+
+
+
+      <div class="div"  id="#registration">
+      <div class="row">
+         <div class="container-fluid">
+  <div class="row no-gutter">
+    <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+    <div class="col-md-8 col-lg-6">
+      <div class="login d-flex align-items-center py-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-9 col-lg-8 mx-auto">
+              <h3 class="login-heading mb-4">Resta connesso</h3>
+              <span class="text"
+            style="letter-spacing: 0.04vw">Registrati per ricevere le nostre
+            offerte</span>
+              <form>
+                <div class="form-label-group">
+                <div class="input-group mb-3">
+  <div class="input-group-prepend">
+  </div>
+  <input type="text" class="form-control" placeholder="Nome" aria-label="Nome" aria-describedby="basic-addon1">
+</div>
+                </div>
+                <div class="input-group mb-3">
+  <div class="input-group-prepend">
+    
+  </div>
+  <input type="text" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1">
+</div>
+            
+<div class="input-group mb-3">
+  <div class="input-group-prepend">
+    
+  </div>
+  <input type="text" class="form-control" placeholder="Telefono" aria-label="Telefono" aria-describedby="basic-addon1">
+</div>
+<button class="btn btn-lg  btn-block btn-login text-uppercase font-weight-bold mb-2 " type="submit">Iscriviti</button>
+            
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+         </div>
+      </div>
+       
+     
+         <div class="row">
+                    <?php include 'html/footer.html'; ?>
+                    </div>
+                    <img data-other-src="Img/Logo_Landscape.png" src="Img/Logo_Landscape.png" style="display:none;">
+    <img data-other-src="Img/Logo_LandscapeHover.png" src="Img/Logo_Landscape.png" style="display:none;">
+
+
+<script src="src/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs/dist/tf.min.js"> </script>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+   
+</body>
+
+</html>
---
+++ promo.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pharmacare</title>
+    <link rel="stylesheet" href="build/css/main.css">
+    <link rel="stylesheet" href="build/css/style.css">
+    <link rel="stylesheet" href="build/css/bootstrap.css">
+    <script src="https://kit.fontawesome.com/9efad85de0.js" crossorigin="anonymous"></script>
+</head>
+
+
+<body >
+
+<div class="container-fluid">
+             <div class="row">
+         <?php include 'html/header.html'; ?>
+         </div>
+               
+         <div class="row">
+         <?php include 'html/promo.html'; ?>
+</div>
+                    <div class="row">
+                    <?php include 'html/footer.html'; ?>
+                    </div>
+         </div>
+                  
+             
+                    <img data-other-src="Img/Logo_Landscape.png" src="Img/Logo_Landscape.png" style="display:none;">
+    <img data-other-src="Img/Logo_LandscapeHover.png" src="Img/Logo_Landscape.png" style="display:none;">
+
+
+<script src="src/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs/dist/tf.min.js"> </script> 
+    <script src="js/jquery.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+   
+</body>
+
+</html>
